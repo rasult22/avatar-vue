83a46f430ac1b569cf97c34cb04410cc3f902113
Itachi-MacOS <warrior.006@inbox.ru>
2024-04-08 17:29:40 +0500

    static files

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig({
-  publicPath: '/avatar-vue/',
-  transpileDependencies: true,
-  lintOnSave: false,
-  devServer: {
-    // https: true
-  }
-});
