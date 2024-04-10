d981839c457ae485e0078c9eccf8e655809f4a32
Itachi-MacOS <warrior.006@inbox.ru>
2024-04-11 00:54:42 +0500

    add

--- js/about.fb7f15c5.js
+++ js/about.fb7f15c5.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkvue_avatar"]=self["webpackChunkvue_avatar"]||[]).push([[594],{3478:function(a,u,n){n.r(u),n.d(u,{default:function(){return f}});var t=n(2003);const e={class:"about"},r=(0,t.Lk)("h1",null,"This is an about page",-1),s=[r];function c(a,u){return(0,t.uX)(),(0,t.CE)("div",e,s)}var v=n(3489);const i={},o=(0,v.A)(i,[["render",c]]);var f=o}}]);
-//# sourceMappingURL=about.a17e00f7.js.map
+//# sourceMappingURL=about.fb7f15c5.js.map
