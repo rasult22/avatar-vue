d74e31154fda003e1014b172145296b54e19c856
Itachi-MacOS <warrior.006@inbox.ru>
2024-04-23 16:12:13 +0500

    ok

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  publicPath: '/avatar-vue/',
+  publicPath: '/',
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
